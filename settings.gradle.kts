57ac26a12178ca0342bddfff0a9b7169595acecb
sajal Tripathi <64830305+sajaltripathi@users.noreply.github.com>
2026-07-13 17:18:50 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,16 @@ pluginManagement {
   }
 }
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
+
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
     google()
     mavenCentral()
+    // Added JitPack to fix the pdfbox-android error
+    maven { url = uri("https://jitpack.io") } 
   }
 }
+
 rootProject.name = "PrivaEdit"
 include(":app")
